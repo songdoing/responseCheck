9862da7e8fb175a1b469ed666bdc5f71ee501ed4
wonjinsong <msmk22239@gmail.com>
2021-01-13 20:56:35 -0500

    webpack-dev-server, babel, react-refresh

--- responseCheckClass.jsx
+++ responseCheckClass.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class ResponseCheck extends Component {
     state = {
-        state : '',
+        state : 'waiting',
         message : 'You need to click for starting a game.',
         result : [],
     };
@@ -11,13 +11,25 @@ class ResponseCheck extends Component {
 
     };
 
+    renderAverage = () => {
+        // 구조분해로 길이 줄이기
+        const { result } =this.state;
+        return (
+            result.length === 0 
+            ? null 
+            :<div>average of response time: {result.reduce((a,c) => a +c) / result.length}ms</div>
+        )
+    };
+
     render() {
+        const { state, message } = this.state;
         return (
             <>
-                <div id="screen" className={this.state.state} onClick={this.onClickScreen}>
-                    {this.state.message}
+                <div id="screen" className={state} onClick={this.onClickScreen}>
+                    {message}
                 </div>
-                <div>average of response time: {this.state.result.reduce((a,c) => a +c) / this.state.result.length}ms</div>
+                {/* false, undefined, null은 jsx에서 태그없음을 의마한다 */}
+                {this.renderAverage()}                
             </>
         );
     }
